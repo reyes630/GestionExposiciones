a0c3c53c897ce275354e66a4c14a9c2b8aee3e6e
juandaniel <juandanielnube@gmail.com>
2024-06-21 09:01:53 -0500

    Se agregó un nuevo archivo

---
+++ gestionExposiciones1.dart
@@ -0,0 +1,57 @@
+void main(List<String> args) {
+  //JUAN DANIEL GÓMEZ Y ESTEBAN REYES - MINI PROYECTO TRIMESTRE 2
+  //DECLARACIÓN DE LISTAS
+  List<String> temas = [
+    "Suma",
+    "Resta",
+    "División",
+    "Multiplicación",
+    "Raiz cuadrada",
+    "Potenciación"
+  ]; //Lista para guardar todos los temas, tiene temas precargados
+  List<String> estudiantes = [
+    "ANDRES FELIPE SANCHEZ HURTADO",
+  "ANGIE DAHIANA RIOS QUINTERO",
+  "CRISTIAN ALVAREZ ARANZAZU",
+  "DANIEL ESTIVEN ARBOLEDA DUQUE",
+  "DAVID ANDRES MORALES GUAPACHA",
+  "DAVID STIVEN OCAMPO LONDOÑO",
+  "ESTEBAN REYES AGUDELO",
+  "JACOBO GALVIS JIMENEZ",
+  "JAIME ANDRES CALLE SALAZAR",
+  "JEFERSON MAURICIO HERNANDEZ LADINO",
+  "JHON ALEXANDER PINEDA OSORIO",
+  "JOSE MIGUEL SIERRA ARISTIZABAL",
+  "JOSÉ SEBASTIÁN OCAMPO LÓPEZ",
+  "JUAN ANDRES OSORIO GOMEZ",
+  "JUAN DIEGO CALVO OSORIO",
+  "JUAN ESTEBAN LOPEZ CALLE",
+  "JUAN PABLO RIOS ARISTIZABAL",
+  "MARIA PAULA MELO SOLANO",
+  "MIGUEL ANGEL PEÑA JIMENEZ",
+  "SAMUEL CASTAÑO CARDONA",
+  "JUAN JOSÉ POSADA PÉREZ",
+  "ALEJANDRO SERNA LONDOÑO",
+  "JUAN MANUEL ZULUAGA RINCON",
+  "JUAN DANIEL GOMEZ LASERNA",
+  "YERSON STIVEN HERRERA OBANDO",
+  "MATEO HERRERA VARGAS",
+  "ALEJANDRO VALLEJO ESCOBAR"
+  ]; //Lista para guardar los nombres de los estudiantes
+  List<int> cantEstudiantes = [
+    3,
+    3,
+    4,
+    4,
+    5,
+    5
+  ]; //Lista para guardar la complejidad de los temas, el número indica la cantidad de estudiantes por tema
+  //DECLARACIÓN DE MATRIZ
+  List<List<String>> asignacion =
+      []; //Matriz que guardará las listas de los estudiantes por tema
+  List<List<String>> historial =
+      []; //Matriz para guardar el historial de las asignaciones
+  //DECLARACIÓN DE VARIABLES
+  int opcion, posicion;
+  String tema, nombre;
+}
